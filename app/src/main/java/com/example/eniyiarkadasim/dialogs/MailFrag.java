81084a253823fde680a701d5c40c4bea8606ffbc
UmutGokce <umutgkc@outlook.com>
2021-06-04 18:42:05 +0300

    RealTimeDatabase read-write Info

--- app/src/main/java/com/example/eniyiarkadasim/dialogs/MailFrag.java
+++ app/src/main/java/com/example/eniyiarkadasim/dialogs/MailFrag.java
@@ -1,6 +1,5 @@
-package com.example.eniyiarkadasim;
+package com.example.eniyiarkadasim.dialogs;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,10 +12,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.eniyiarkadasim.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MailFrag extends Fragment {
